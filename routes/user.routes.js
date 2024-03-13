562e87af4d13c5b93c6e9e4eaf03f56766dd9334
Aagam Doshi <aagamdoshi1100@gmail.com>
2024-03-13 18:23:00 +0530

    feat: endpoint for user account deletion

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -88,4 +88,22 @@ userRouter.put("/:userId/edit", tokenVerify, async (req, res) => {
   }
 });
 
+userRouter.delete("/:userId/delete", tokenVerify, async (req, res) => {
+  try {
+    const userRes = await user.findByIdAndDelete(req.params.userId);
+    if (!userRes) {
+      res.status(404).json({
+        message: `User ${req.params.userId} not found or may be already deleted`,
+      });
+    } else {
+      res.status(200).json({
+        message: `User ${userRes.username} has been deleted`,
+        userId: req.params.userId,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = userRouter;
